b8bb124a02a506f411c6f693bd5e05c1986d0716
Liam Greenway <ApplePiePc@outlook.com>
2025-04-08 07:46:29 -0400

    The font measuring update

--- src/text.rs
+++ src/text.rs
@@ -1,10 +1,12 @@
-use crate::types::Color;
+use crate::types::{Color};
 use fontdue_sdl2::fontdue::FontSettings;
 use fontdue_sdl2::fontdue::layout::{CoordinateSystem, Layout, TextStyle};
 use fontdue_sdl2::{FontTexture, fontdue};
 use sdl2::pixels;
 use std::fs::File;
 use std::io::Read;
+use sdl2::render::Canvas;
+use sdl2::sys::Window;
 use crate::core::MAYLIB;
 
 pub struct Font {
@@ -48,5 +50,55 @@ pub fn draw_text(font: &Font, text: &str, size: f32, x: i32, y: i32, color: Colo
     if let Some(window) = get.windows.get_mut(&current_window) {
         let mut font_texture = FontTexture::new(&window.texture).expect("Failed to load font");
         let _ = font_texture.draw_text(&mut window.canvas, &fonts, layout.glyphs().as_slice());
+    }
+}
+
+pub fn measure_text_width(font: &Font, text: &str, size: f32) -> f32 {
+    let mut get = MAYLIB.lock().expect("Should be able to lock");
+    let current_window = get.current_window;
+    let mut layout: Layout<Color> = Layout::new(CoordinateSystem::PositiveYDown);
+    let fonts = [font.font.clone()];
+
+    layout.reset(&fontdue::layout::LayoutSettings {
+        x: 0f32, // Apply X offset
+        y: 0f32, // Apply Y offset
+        ..Default::default()
+    });
+
+    layout.append(
+        &fonts,
+        &TextStyle::with_user_data(text, size, 0, Color::MayGray.into()),
+    );
+    
+    if let Some(window) = get.windows.get_mut(&current_window) {
+        FontTexture::new(&window.texture).expect("Failed to load font").texture.query().width as f32
+    }
+    else { 
+        return 0.0;
+    }
+}
+
+pub fn measure_text_height(font: &Font, text: &str, size: f32) -> f32 {
+    let mut get = MAYLIB.lock().expect("Should be able to lock");
+    let current_window = get.current_window;
+    let mut layout: Layout<Color> = Layout::new(CoordinateSystem::PositiveYDown);
+    let fonts = [font.font.clone()];
+
+    layout.reset(&fontdue::layout::LayoutSettings {
+        x: 0f32, // Apply X offset
+        y: 0f32, // Apply Y offset
+        ..Default::default()
+    });
+
+    layout.append(
+        &fonts,
+        &TextStyle::with_user_data(text, size, 0, Color::MayGray.into()),
+    );
+
+    if let Some(window) = get.windows.get_mut(&current_window) {
+        FontTexture::new(&window.texture).expect("Failed to load font").texture.query().height as f32
+    }
+    else { 
+        return 0.0;
     }
 }
